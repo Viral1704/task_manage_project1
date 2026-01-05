fd46ba062265e35d3ab0e5c4a5afae8637c978b0
Viral_1704 <viralvaghasiya8@gmail.com>
2026-01-05 10:13:15 +0530

    Till now task table created in model.

--- app/models.py
+++ app/models.py
@@ -21,3 +21,11 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable = False)
+    description = db.Column(db.Text, nullable = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    status = db.Column(db.String(20), nullable = False, default='pending')
